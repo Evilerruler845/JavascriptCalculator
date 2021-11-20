ae5308e4864f51c49a432b0d5b45a0a6964baef1
Evilerruler845 <75455747+Evilerruler845@users.noreply.github.com>
2021-11-19 21:54:12 -0800

    Fixed many bugs.

--- CSP-calculator.js
+++ CSP-calculator.js
@@ -42,50 +42,91 @@ document.addEventListener("DOMContentLoaded", function() {
         const firstValue = calculator.dataset.firstValue
         const operator = calculator.dataset.operator
         const secondValue = displayedNum
+        const previousKeyType = calculator.dataset.previousKeyType
+
+        if (
+          firstValue &&
+          operator &&
+          previousKeyType !== 'operator' &&
+          previousKeyType !== 'calculate'
+        ) {
+          const calcValue = calculate(firstValue, operator, secondValue)
+          display.textContent = calcValue
+          
+        // Update calculated value as firstValue
+          calculator.dataset.firstValue = calcValue
+        } else {
+          // If there are no calculations, set displayedNum as the firstValue
+          calculator.dataset.firstValue = displayedNum
+        }
 
         key.classList.add('is-depressed')   
         // Add custom attribute
         calculator.dataset.previousKeyType = 'operator'
-        calculator.dataset.firstValue = displayedNum
         calculator.dataset.operator = action                   
       }
       
       if (action === 'decimal') {
         console.log('decimal key!')
-        display.textContent = displayedNum + '.'
+        if (calculator.dataset.previousKeyType === 'operator' ||
+          calculator.dataset.previousKeyType === 'calculate') {
+          display.textContent = '0.'
+        } else if (!displayedNum.includes('.')) {
+          display.textContent = displayedNum + '.'
+        }
         calculator.dataset.previousKeyType = 'decimal'
       }
       
       if (action === 'clear') {
         console.log('clear key!')
+
+        if (key.textContent === 'AC') {
+          calculator.dataset.firstValue = ''
+          calculator.dataset.modValue = ''
+          calculator.dataset.operator = ''
+          calculator.dataset.previousKeyType = ''
+        } else {
+          key.textContent = 'AC'
+        }
+
+        display.textContent = 0      
         calculator.dataset.previousKeyType = 'clear'
       }
       
       if (action === 'calculate') {
         console.log('equal key!')
 
-        const firstValue = calculator.dataset.firstValue
+        let firstValue = calculator.dataset.firstValue
         const operator = calculator.dataset.operator
-        const secondValue = displayedNum
+        let secondValue = displayedNum
         
         if (firstValue) {
+          if (calculator.dataset.previousKeyType === 'calculate') {
+            firstValue = displayedNum
+            secondValue = calculator.dataset.modValue
+          }
           display.textContent = calculate(firstValue, operator, secondValue)
         }
-
+        calculator.dataset.modValue = secondValue
         calculator.dataset.previousKeyType = 'calculate'    
       }
       
       const previousKeyType = calculator.dataset.previousKeyType
 
       if (!action) {
-        if (displayedNum === '0' || previousKeyType === 'operator') {
+        if (displayedNum === '0' || 
+        previousKeyType === 'operator' ||
+        previousKeyType === 'calculate') {
           display.textContent = keyContent
         } else {
             display.textContent = displayedNum + keyContent        
         }
+        calculator.dataset.previousKeyType = 'number'        
       }
-      if (action === 'decimal') {
-        display.textContent = displayedNum + '.'
+
+      if (action !== 'clear') {
+        const clearButton = calculator.querySelector('[data-action=clear]')
+        clearButton.textContent = 'CE'
       }
 
       Array.from(key.parentNode.children)
